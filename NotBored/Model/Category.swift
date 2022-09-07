b537f46a746b97a2c23ba047150af8b345804a67
Nicolas Ferrero <nicolas.nferrero@mercadolibre.com>
2022-09-07 11:36:00 -0300

    Se crea la tabBar que aloja la lista de categorias y la categoria random como tambien se agregan los modelos

---
+++ NotBored/Model/Category.swift
@@ -0,0 +1,12 @@
+//
+//  Categories.swift
+//  NotBored
+//
+//  Created by Nicolas Alejandro Ferrero on 06/09/2022.
+//
+
+import Foundation
+
+enum Category: String, CaseIterable {
+    case education, recreational, social, diy, charity, cooking, relaxation, music, busywork
+}
